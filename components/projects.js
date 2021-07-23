107b4d722296a47efe6b84a3ff8768e52ec9cbdd
anthonyrovira <anthonyrov@gmail.com>
2021-07-23 22:36:08 +0200

    update: projects

--- components/projects.js
+++ components/projects.js
@@ -1,45 +1,30 @@
-import React, { useState } from "react";
-import Image from "next/image";
-import projects from "@content/json/ar-projects.json";
+import React, { useState, useContext } from "react";
+import ProjectsGrid from "@components/ProjectsGrid";
+import ProjectFocus from "@components/ProjectFocus";
+import { LanguageContext } from "@contexts/langContext";
 import styles from "@styles/Projects.module.css";
 
-const Projects = ({ content, language }) => {
-  const [gridView, setGridView] = useState(true);
+const Projects = () => {
+  const [toggleGridView, setToggleGridView] = useState(true);
+  const [targetedProject, setTargetedProject] = useState();
 
-  const { title } = content;
+  const language = useContext(LanguageContext);
+
+  const { title } = language.main.projects;
+
   return (
     <section className={styles.projectsContainer}>
       <h1 className={styles.title}>{title}</h1>
-      {gridView ? (
-        <div className={styles.gridContainer}>
-          {projects.map((project) => {
-            return (
-              <div key={project.id} className={styles.cardProjectContainer}>
-                <div className={styles.thumbnailContainer}>
-                  <Image
-                    className={styles.projectThumbnail}
-                    src={project.thumbnail}
-                    layout={"fill"}
-                    objectFit={"cover"}
-                    /*width={318}
-                    height={187}*/
-                    quality={100}
-                    alt={project.title}
-                  />
-                </div>
-
-                <p className={styles.projectCategory}>{project.category}</p>
-              </div>
-            );
-          })}
-        </div>
+      {toggleGridView ? (
+        <ProjectsGrid
+          setToggleGridView={setToggleGridView}
+          setTargetedProject={setTargetedProject}
+        />
       ) : (
-        <div className={styles.descriptionContainer}>
-          <div className={styles.description}>
-            <h2>Le titre</h2>
-          </div>
-          <div className={styles.carouselContainer}></div>
-        </div>
+        <ProjectFocus
+          projectData={language.projects[targetedProject]}
+          slug={language.projects[targetedProject].slug}
+        />
       )}
     </section>
   );
