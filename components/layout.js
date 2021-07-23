ce6d7eaefc591b3b51f09e34ff48e2a12967ebe7
anthonyrovira <anthonyrov@gmail.com>
2021-07-23 22:26:02 +0200

    update: Layout component

--- components/layout.js
+++ components/layout.js
@@ -3,23 +3,22 @@ import Head from "next/head";
 import Header from "./header";
 import Footer from "./footer";
 import styles from "@styles/Layout.module.css";
+import { LanguageContext, languages } from "@contexts/langContext";
 
-export default function Layout({ children, sideMenu, setSideMenu, language }) {
+export default function Layout({ children, sideMenu, setSideMenu }) {
   return (
     <div className={styles.root}>
-      <Head>
-        <title>Home | Next.js</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-      <Header
-        sideMenu={sideMenu}
-        setSideMenu={setSideMenu}
-        language={language}
-      />
+      <LanguageContext.Provider value={languages.english}>
+        <Head>
+          <title>Portfolio | Anthony Rovira</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <Header sideMenu={sideMenu} setSideMenu={setSideMenu} />
 
-      <main className={styles.mainContainer}>{children}</main>
+        <main className={styles.mainContainer}>{children}</main>
 
-      <Footer language={language} />
+        <Footer />
+      </LanguageContext.Provider>
     </div>
   );
 }
